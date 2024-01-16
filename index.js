00fbb0f36c8607655635f08c29ab23914003ddd5
cat coder <s.emama@hotmail.com>
2024-01-16 23:15:00 +0000

    added some comments

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const questions = [
       return "Please add an answer"
     }
     }
+    //Validation ensure user inputs an answer to move on
   },
   {
     type: 'input',
@@ -115,53 +116,17 @@ const questions = [
 // function to write README file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  // writes the data given to specified path and joins to current working directory with given name filename.
 }
-// Create a command-line application that accepts user input.
-// * When a user is prompted for information about the application repository then a high-quality, professional README.md is generated with:
-//   * The title of my project 
-//   * Sections entitled:
-//     * Description 
-//     * Table of Contents 
-//     * Installation 
-//     * Usage 
-//     * License 
-//     * Contributing 
-//     * Tests 
-//     * Questions
-//   * When a user enters the project title then it is displayed as the title of the README
-//   * When a user enters a description, installation instructions, usage information, contribution guidelines, and test instructions then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-//   * When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled **License** that explains which license the application is covered under
-//   * When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
-//   * When a user enters their email address then this is added to the section of the README entitled Questions, with instructions on how to reach them with additional questions
-//   * When a user clicks on the links in the **Table of Contents** then they are taken to the corresponding section of the README
 
 // function to initialize program
 function init() {
   inquirer.prompt(questions).then((answers) => {
     console.log('Generating README');
-     writeToFile("./read/README.md", generateMarkdown({ ...answers }));
+     writeToFile("./read/README.md", generateMarkdown({ ...answers })); 
+     // Creates a readme file and puts it into the read folder and allows answers to be stored into that folder
   });
 } 
 
-
-// const promptUser = async () => {
-//   try {
-//    return await inquirer.prompt([
-    
-    
-// // Using async/await and try/catch
-// const init = async () => {
-//     console.log('Generating README');
-//     try {
-//       const answers = await promptUser();
-//       const readmeContent = generateMarkdown(answers);
-//       writeToFile('README.md', readmeContent);
-  
-//       console.log('Successfully wrote to README,md');
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-// function call to initialize program
 
 init();
